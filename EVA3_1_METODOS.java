882775004c81ecb6e735943c4ee59dc89b13c7d4
dana-meraz <L24550372@chihuahua2.tecnm.mx>
2024-11-04 16:51:44 -0600

    Add files via upload

---
+++ EVA3_1_METODOS.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_1_metodos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //INVOCAE EL MÉTODO, LLAMAR A METODO ---> USARLO
+        imprimirMensaje("HOLA MUNDO");
+        
+    }
+    //declaración del metodo
+    //tiene que empesar en minusculas y si tiene otra palabla empiesa con mallu
+    //Modificaciones (OPCIONALES)
+        //VALORES RETORNO
+            //NOMBRE       
+                         //PARAMETROS (OPCIONALES)
+    
+    //SON UTILES PARA REUTILIZAR CÓDIGO
+                             //ESTA ES LA FIRMA LA FIRMA NO SE PUEDE REPETIR 
+    public static void imprimirMensaje(String mensaje){
+      //cuerpo del metodo
+  System.out.println("Tu mensaje: " + mensaje);
+  
+  
+  }
+    
+    
+}
